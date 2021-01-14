cb99eec649ffe15931e4ff7991bfd04effc23521
Oguzhan-altunbass <ognx.xx.x@gmail.com>
2021-01-14 19:28:42 +0300

    kasa ve müşteri sınıfları oluşturuldu

---
+++ gür market/sınıflar/Kasa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gür_market.sınıflar
+{
+    public class Kasa
+    {
+        public int ToplamBorc { get; set; }
+        public int ToplamAlacakMiktari { get; set; }
+        public int PotansiyelKarZarar { get; set; }
+        public int MevcutKasaMiktari { get; set; }
+    }
+}
---
+++ gür market/sınıflar/Musteri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gür_market.sınıflar
+{
+    public class Musteri
+    {
+        public int MusteriId { get; set; }
+        public string MusteriAdSoyad { get; set; }
+        public int MusteriTel { get; set; }
+        public string TarihSaat { get; set; }
+        public int BorcMiktari { get; set; }
+    }
+}
